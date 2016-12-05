7228f0a3e4bb3e58819d72698b03aff9a76c4de2
Bradley Boehmke <bradleyboehmke@gmail.com>
2016-12-05 15:06:57 -0500

    Data & vignette
    Added psc example dataset and expanded the vignette

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Product and service contracts
+#'
+#' A dataset containing a single value score for the x attribute (i.e. supply risk)
+#' and y attribute (i.e. profit impact) of 200 product and service contracts (PSC).
+#' The variables are as follows:
+#'
+#' @format A tibble with 200 rows and 3 variables:
+#' \describe{
+#'   \item{PSC}{Contract identifier for each product and service}
+#'   \item{x_attribute}{x attribute score, from 1 (worst) to 5 (best)}
+#'   \item{y_attribute}{y attribute score, from 1 (worst) to 10 (best)}
+#' }
+"psc"

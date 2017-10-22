b40688c5a47374e8d6dee8d628e7cc5f66be0b16
Brad Boehmke <bradleyboehmke@gmail.com>
2017-10-22 18:15:14 -0400

    finished get_frontier function and starting building geom_frontier

--- R/geom_frontier.R
+++ R/geom_frontier.R
@@ -1,15 +1,26 @@
 #' Get values on the efficient frontier
 #'
 #' \code{get_frontier} identifies and extracts the \code{x} and \code{y} values
-#' that fall along the user defined efficient frontier.
+#' that fall along the user defined efficient frontier. The efficient frontier
+#' can be defined by a combination of large \code{x} and large \code{y} values,
+#' large \code{x} and small \code{y} values, small \code{x} and large \code{y}
+#' values, or small \code{x} and small \code{y} values.
 #'
 #' @param data A data frame
 #' @param x Numeric vector of values
 #' @param y Numeric vector of values
 #' @param quadrant Depending on which direction on each axes defines "better",
-#' it places the frontier in the appropriate quadrant.
+#' it places the frontier in the appropriate quadrant. For example, if large
+#' \code{x} and large \code{y} values are considered "better" then the default
+#' "top.right" is appropriate.  If small \code{x} and small \code{y} are
+#' considered "better" then "bottom.left" is appropriate.
+#' @param decreasing logical. Should the sort order be increasing or decreasing?
 #'
-#' @return A data frame with the efficient frontier observations
+#' @return A data frame with the efficient frontier observations.
+#'
+#' @seealso
+#'
+#' \code{\link{geom_frontier}} for plotting the efficient frontier.
 #'
 #' @examples
 #'
@@ -24,8 +35,10 @@
 #'
 #' @export
 
-get_frontier <- function(data, x, y, quadrant = c("top.right", "bottom.right",
-                                            "bottom.left", "top.left")) {
+get_frontier <- function(data, x, y,
+                         quadrant = c("top.right", "bottom.right",
+                                            "bottom.left", "top.left"),
+                         decreasing = TRUE) {
   # create subsetted data frame
   x_col <- deparse(substitute(x))
   y_col <- deparse(substitute(y))
@@ -44,23 +57,33 @@ get_frontier <- function(data, x, y, quadrant = c("top.right", "bottom.right",
     zz <- z[order(z[[x_col]], z[[y_col]], decreasing = TRUE), ]
 
     # identify the efficient frontier & remove duplicates
-    frontier <- which(cummax(zz[y_col]) <= zz[y_col])
-    frontier <- frontier[!duplicated(zz[frontier, ])]
+    frontier <- cummax(zz[y_col])
+    frontier <- which(!duplicated(frontier))
 
     # return efficient frontier values
-    zz[frontier, ]
+    zz <- zz[frontier, ]
+    zz[order(zz[[x_col]], zz[[y_col]], decreasing = decreasing), ]
 
   } else if (quadrant == "bottom.right") {
 
     # order data frame
     zz <- z[order(z[[x_col]], z[[y_col]], decreasing = TRUE), ]
 
-    # identify the efficient frontier & remove duplicates
-    frontier <- which(cummin(zz[y_col]) >= zz[y_col])
-    frontier <- frontier[!duplicated(zz[frontier, ])]
+    # identify the efficient frontier for y values & remove duplicates
+    frontier <- cummin(zz[y_col])
+    frontier <- which(!duplicated(frontier))
 
     # return efficient frontier values
-    zz[frontier, ]
+    zz <- zz[frontier, ]
+
+    # remove any duplicate x values
+    zz <- zz[order(zz[[y_col]], zz[[x_col]], decreasing = FALSE), ]
+    frontier_x <- cummax(zz[x_col])
+    frontier_x <- which(!duplicated(frontier_x))
+
+    # return efficient frontier values in appropriate order
+    zz <- zz[frontier_x, ]
+    zz[order(zz[[x_col]], zz[[y_col]], decreasing = decreasing), ]
 
   } else if (quadrant == "bottom.left") {
 
@@ -68,13 +91,123 @@ get_frontier <- function(data, x, y, quadrant = c("top.right", "bottom.right",
     zz <- z[order(z[[x_col]], z[[y_col]], decreasing = FALSE), ]
 
     # identify the efficient frontier & remove duplicates
-    frontier <- which(cummin(zz[y_col]) >= zz[y_col])
-    frontier <- frontier[!duplicated(zz[frontier, ])]
+    frontier <- cummin(zz[y_col])
+    frontier <- which(!duplicated(frontier))
+
+    # return efficient frontier values
+    zz <- zz[frontier, ]
+    zz[order(zz[[x_col]], zz[[y_col]], decreasing = decreasing), ]
 
   } else {
-    zz <- z[order(z[, 1L], z[, 2L], decreasing = FALSE), ]
-    zz[which(!duplicated(cummax(zz[, 2L]))), ]
+
+    # order data frame
+    zz <- z[order(z[[x_col]], z[[y_col]], decreasing = FALSE), ]
+
+    # identify the efficient frontier & remove duplicates
+    frontier <- cummax(zz[y_col])
+    frontier <- which(!duplicated(frontier))
+
+    # return efficient frontier values
+    zz <- zz[frontier, ]
+
+    # remove any duplicate x values
+    zz <- zz[order(zz[[y_col]], zz[[x_col]], decreasing = TRUE), ]
+    frontier_x <- cummin(zz[x_col])
+    frontier_x <- which(!duplicated(frontier_x))
+
+    # return efficient frontier values in descending order
+    zz <- zz[frontier_x, ]
+    zz[order(zz[[x_col]], zz[[y_col]], decreasing = decreasing), ]
   }
 }
 
 
+#' Plot values on the efficient frontier with a ggplot2 object
+#'
+#' \code{geom_frontier} plots the \code{x} and \code{y} values
+#' that fall along the user defined efficient frontier. The efficient frontier
+#' can be defined by a combination of large \code{x} and large \code{y} values,
+#' large \code{x} and small \code{y} values, small \code{x} and large \code{y}
+#' values, or small \code{x} and small \code{y} values.
+#'
+#' @param mapping Set of aesthetic mappings created by \code{aes()} or
+#'   \code{aes()}. If specified and `inherit.aes = TRUE` (the
+#'   default), it is combined with the default mapping at the top level of the
+#'   plot. You must supply `mapping` if there is no plot mapping.
+#' @param data The data to be displayed in this layer. There are three
+#'    options:
+#'
+#'    If `NULL`, the default, the data is inherited from the plot
+#'    data as specified in the call to `ggplot()`.
+#'
+#'    A `data.frame`, or other object, will override the plot
+#'    data. All objects will be fortified to produce a data frame. See
+#'    [fortify()] for which variables will be created.
+#'
+#'    A `function` will be called with a single argument,
+#'    the plot data. The return value must be a `data.frame.`, and
+#'    will be used as the layer data.
+#' @param x Numeric vector of values
+#' @param y Numeric vector of values
+#' @param quadrant Depending on which direction on each axes defines "better",
+#' it places the frontier in the appropriate quadrant. For example, if large
+#' \code{x} and large \code{y} values are considered "better" then the default
+#' "top.right" is appropriate.  If small \code{x} and small \code{y} are
+#' considered "better" then "bottom.left" is appropriate.
+#' @param direction direction of stairs: 'vh' for vertical then horizontal, or
+#' 'hv' for horizontal then vertical
+#' @param ... other arguments passed on to layer. These are often aesthetics,
+#' used to set an aesthetic to a fixed value, like `color = "red"` or `size = 3`.
+#'
+#' @section Aesthetics:
+#'
+#' `geom_frontier` understands the following aesthetics:
+#'     \enumerate{
+#'       \item \code{x}:
+#'       \item \code{y}:
+#'       \item \code{alpha}:
+#'       \item \code{color}:
+#'       \item \code{linetype}:
+#'       \item \code{Size}:
+#'     }
+#'
+#' @seealso
+#'
+#' \code{\link{geom_frontier}} for identifying and extracting the observations that
+#' lie along the efficient frontier.
+#'
+#' @examples
+#'
+#' @rdname geom_frontier
+
+geom_frontier <- function(mapping = NULL, data = NULL, stat = "identity",
+                          position = "identity", direction = "vh",
+                          na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, ...) {
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = GeomFrontier,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      direction = direction,
+      na.rm = na.rm,
+      ...
+    )
+  )
+}
+
+#' @rdname ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+GeomFrontier <- ggproto("GeomFrontier", GeomStep,
+                        setup_data = function(data, direction = "hv") {
+                          data <- get_frontier(data, x, y)
+                          data$PANEL = 1
+                          data$group = -1
+                          data[order(data$PANEL, data$group, data$x), ]
+                        }
+)
